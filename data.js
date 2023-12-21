9cb9b61f70a0d8f397f837e51d3ffc59e3cebe65
Nik-tec <niks.bhatts.7505468@gmail.com>
2023-12-22 03:50:11 +0530

    This is for testig purpose

---
+++ data.js
@@ -0,0 +1,24 @@
+const arr= [
+    {
+        name : 'app1.txt',
+        content : "aap1 me aa gya"
+    },
+    {
+        name : 'app2.txt',
+        content : "aap2 me aa gya"
+    },
+    {
+        name : 'app3.txt',
+        content : "aap3 me aa gya"
+    },
+    {
+        name : 'app4.txt',
+        content : "aap4 me aa gya"
+    },
+    {
+        name : 'app5.txt',
+        content : "aap5 me aa gya"
+    },
+];
+
+module.exports = arr;
---
+++ index.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+
+app.use(express.static(__dirname+'/public'));
+
+app.get('',(req, res)=>{
+    res.render(__dirname+"/public/index.html");
+})
+
+
+app.get('*', (req, res)=>{
+     res.sendFile(__dirname+"/public/error_404.html")}
+);
+    
+
+
+app.listen(port, (err)=>{
+    try{
+      console.log("Listen");
+    }catch(err){
+        console.log(err);
+    }
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const fs = require('fs');
+// const path = require('path');
+// const http = require('http');
+// const arr = require('./data.js');
+
+
+// http.createServer((req, res)=>{
+
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+//     res.write("hi");
+
+//     res.end();
+
+// }).listen(3000)
+// const dname = path.join(__dirname, 'files/');
+
+
+// // arr.forEach((filenames)=>{
+// //     fs.writeFileSync(dname + filenames.name,filenames.content);
+// // });
+
+
